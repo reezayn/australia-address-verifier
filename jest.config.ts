6eac652f0a6e0dab663d0c5c35fb2bafdce53843
reezayn <rijanshrestha98450@gmail.com>
2025-02-16 21:50:00 +0545

    Set up Jest and React Testing Library, add unit tests for AddressForm component

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react|react-dom|@apollo/client|@testing-library)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+export default config
