88c33507fc4e14bbf63801fd57ad41b4c0fa1514
reezayn <rijanshrestha98450@gmail.com>
2025-02-17 18:35:00 +0545

    Add project info component and update favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import AddressVerifier from '@/components/AddressVerifier'
+import ProjectInfo from '@/components/ProjectInfo'
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full p-3 flex flex-col items-center justify-center bg-tangerine relative">
       <AddressVerifier />
+
+      <ProjectInfo />
     </div>
   )
 }
